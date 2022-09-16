62609ace469c208fcb007deee5a9de6c92238b5c
PrajulJaggi577 <prajul171577.cse@chitkara.edu.in>
2022-09-16 12:36:59 -0700

    Added Toast, Light/Dark theme

--- src/App.js
+++ src/App.js
@@ -3,28 +3,43 @@ import Navbar from '../src/components/Navbar';
 import TextForm from './components/TextForm';
 // import About from './components/About';
 import React, { useState } from 'react'
+import Alert from './components/Alert';
 
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
 
-  const [mode,setMode] = useState('light');
-  
   const toggleMode = () => {
-    if(mode === 'light') {
+    if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor='#343a40';
+      document.body.style.backgroundColor = '#343a40';
+      showAlert("Dark Mode has been enabled","success")
     } else {
       setMode('light');
-      document.body.style.backgroundColor='white';
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been enabled","success")
     }
+  }
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    },2000)
+
   }
   return (
     <>
-    <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode}/>
-    <div className="container">
-      <TextForm heading="Enter text down here" mode={mode}/>
-      {/* <About/> */}
-    </div>
+      <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container">
+        <TextForm showAlert={showAlert} heading="Enter text down here" mode={mode} />
+        {/* <About/> */}
+      </div>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,21 +6,25 @@ export default function TextForm(props) {
     const handleUppClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to Uppercase","success");
     }
 
     const handleLowClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to Lowercase","success");
     }
 
     const handleClear = () => {
         let newText = '';
         setText(newText);
+        props.showAlert("Text Cleared","success");
     }
 
     const handleSpaces = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
+        props.showAlert("Removed Extra Spaces","success");
     }
     const handleOnChange = (event) => {
         setText(event.target.value);
@@ -28,15 +32,16 @@ export default function TextForm(props) {
 
     return (
         <>
-        <div className='conainer' style={{backgroundColor: props.mode==='light'?'white':'#343a40', color: props.mode==='light'?'black':'white'}}>
+        <div className='conainer text-center' style={{backgroundColor: props.mode==='light'?'white':'#343a40', color: props.mode==='light'?'black':'white'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'white':'#343a40', color: props.mode==='light'?'black':'white'}} id="exampleFormControlTextarea1" placeholder='Enter text here' rows="7"></textarea>
             </div>
             <button className="btn btn-primary mx-1 my-1" onClick={handleUppClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-1 my-1" onClick={handleLowClick}>Convert to Lowercase</button>
-            <button className="btn btn-primary mx-1 my-1" onClick={handleClear}>Clear Text</button>
             <button className="btn btn-primary mx-1 my-1" onClick={handleSpaces}>Remove Extra Spaces</button>
+            <button className="btn btn-danger mx-1 my-1" onClick={handleClear}>Clear Text</button>
+            
         </div>
         <div className="container my-5" style={{backgroundColor: props.mode==='light'?'white':'#343a40', color: props.mode==='light'?'black':'white'}}>
             <h2>Text Summary</h2>
