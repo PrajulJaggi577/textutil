da0fe5a120accade923f2a9600b62ea081924552
PrajulJaggi577 <prajul171577.cse@chitkara.edu.in>
2022-09-17 17:00:16 +0530

    added extra functions,react-router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 
 export default function Navbar(props) {
@@ -6,7 +7,7 @@ export default function Navbar(props) {
     <div>
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">{props.title}</a>
+          <Link className="navbar-brand" to="/">{props.title}</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -16,7 +17,7 @@ export default function Navbar(props) {
                 <a className="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">{props.aboutText}</a>
+                <a className="nav-link" href="/about">{props.aboutText}</a>
               </li>
             </ul>
             <div className={`form-check form-switch text-${props.mode === 'light'?'dark':'light'}`}>
